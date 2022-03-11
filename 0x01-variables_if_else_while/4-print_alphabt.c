837ddecd05a660c4eb5dc13430e7ab8fcc7e8d60
Segun-Philip <philipojediran@gmail.com>
2022-03-11 21:10:30 +0100

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
