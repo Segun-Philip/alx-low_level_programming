200ea28791edc22c2bf7ff5dc2af43d2f99e00bf
Segun-Philip <philipojediran@gmail.com>
2022-03-11 18:50:37 +0100

    print lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints lower case alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+}
