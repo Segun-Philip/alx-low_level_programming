82170caaa418a92bea4ce44f52baba30e84c0162
Ojediran Philip Segun <97899418+Segun-Philip@users.noreply.github.com>
2022-03-11 03:11:27 +0100

    Update 4-puts.c
    modification

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {	
-	const char hello[] = "\"Programming is like building a multilingual puzzle\n";
-
+	const char hello[] = "\"Programming is like building a multilingual puzzle";
 	puts(hello);
 	return (0);
 }
