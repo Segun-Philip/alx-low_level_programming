0f424a5a940b0e19b99672e924a55adb9d9e999e
Segun-Philip <philipojediran@gmail.com>
2022-03-11 03:22:51 +0100

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/**
+
+i/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	const char hello[] = "\"Programming is like building a multilingual puzzle\n";
+{
+	const char hello[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(hello);
 	return (0);
