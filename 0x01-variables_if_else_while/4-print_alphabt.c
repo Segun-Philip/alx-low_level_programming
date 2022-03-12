9b2a0d1407187d04ad265c9d96cc889203a60a2a
Segun-Philip <philipojediran@gmail.com>
2022-03-12 01:41:01 +0100

    remove e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-	putchar('\n');
+	for (i = 97; i <= 122; i++)
+		if (i != 101 && i != 113)
+			putchar(i);
+	putchar(10);
 
 	return (0);
 }
