bf97f50b1ec88e53444df909a8ec762781342dfe
Segun-Philip <philipojediran@gmail.com>
2022-03-12 01:09:24 +0100

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ *
+ * main - Entry point
+ *
+ * Description: print 1 to 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar(i);
+
+	return (0);
+}
