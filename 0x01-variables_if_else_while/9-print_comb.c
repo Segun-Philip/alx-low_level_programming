6a824023db5ad224bf5639d30f411e90fa741689
Segun-Philip <philipojediran@gmail.com>
2022-03-12 02:53:30 +0100

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: assigns random number to variable n
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+
+	return (0);
+}
