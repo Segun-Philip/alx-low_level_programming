ced1cd977a12dbbd02cb3e2a156b4e66bb05d2e3
Segun-Philip <philipojediran@gmail.com>
2022-03-17 23:49:54 +0100

    is sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - Print the sign of a number
+ *
+ * @n: type integer, can take positive or negative arguments
+ *
+ * Description: print +, -, or 0
+ *
+ * Return: (1) if +, (0) if 0, and (-1) if -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
