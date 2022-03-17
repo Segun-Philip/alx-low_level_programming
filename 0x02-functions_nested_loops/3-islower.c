54929571e9bfcc107d6c2c46b63aeb883fa41d43
Segun-Philip <philipojediran@gmail.com>
2022-03-17 23:24:10 +0100

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - return 1 or 0
+ * 
+ * Description: prints all lowercase letters
+ *
+ * Return: (0)
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
