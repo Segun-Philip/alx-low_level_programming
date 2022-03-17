080ca8c5dd6becb4f6da67656876470aa0611a8e
Segun-Philip <philipojediran@gmail.com>
2022-03-17 13:02:19 +0100

    minor changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,16 @@
 #include "main.h"
-/*
+
+/**
  * main - Entry point
  *
  * Description: print _putchar
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
