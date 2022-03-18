56cffcd526c37c66f0ef2b16ce41beeeed734516
Segun-Philip <philipojediran@gmail.com>
2022-03-18 03:39:22 +0100

    had to push all once

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,9 +2,8 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character '_' should be printed
+ * @n: number of times the character _ should be printed
  */
-
 void print_line(int n)
 {
 	if (n <= 0)
